44242ad8392433e44db569de62e740613a63a60f
VISHAL KUMAR <vishal.kumar061@gmail.com>
2026-05-13 20:30:48 +0000

    2

--- models/date_dimentions.sql
+++ models/date_dimentions.sql
@@ -1,10 +1,13 @@
+{{ config(materialized='table') }}
+
 WITH cte as(
     select 
   TO_TIMESTAMP(replace(STARTTIME,'"',''))  as STARTED_AT  ,
   Date(TO_TIMESTAMP(replace(STARTTIME,'"','')))  as DATE_STARTED_AT ,
   hour(TO_TIMESTAMP(replace(STARTTIME,'"','')))  as HOUR_STARTED_AT , 
-  case when dayname(TO_TIMESTAMP(replace(STARTTIME,'"',''))) in ('Sun','Sat') then 'WEEKEND' else 'BUSINESS DAY' end as DAY_TYPE  , 
+   {{DAY_TYPE('STARTTIME')}},
   'SPRING' as STATION_OF_YEAR
+
     from {{ source('demo', 'bike') }}
     where replace(STARTTIME,'"','') != 'starttime' 
     limit 10
